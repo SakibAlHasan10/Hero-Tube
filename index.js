01364556c326ed50b08ad7ce184d182692139357
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-01 01:29:24 +0600

    responsive

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const handelCategory = (allData) => {
   allData.forEach((data) => {
     const span = document.createElement("span");
     span.innerHTML = `
-        <button onclick="handelAllCategory(${data.category_id})" class="btn mr-6 btn-active  text-md font-medium capitalize">${data.category}</button>
+        <button onclick="handelAllCategory(${data.category_id})" class="btn mr-3 md:mr-6 btn-active  text-md font-medium capitalize">${data.category}</button>
         `;
     allButtonContainer.appendChild(span);
     // console.log(data)
@@ -34,7 +34,7 @@ const handelAllCategory = async (id) => {
 // handle card
 const handleCard = (card) => {
     const allCartContainer = document.getElementById("all-card");
-    const badge = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-blue-600"><path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" /></svg>'
+    const badge = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-blue-600"><path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" /></svg>'
   
     allCartContainer.innerHTML= '';
   card.forEach((data) => {
@@ -47,18 +47,10 @@ const handleCard = (card) => {
                     <img class="h-10 w-10 rounded-full" src="${data?.authors[0]?.profile_picture}">
                     <div>
                             <h2 class="card-title">${data?.title}</h2>
-                            <p class="my-2">${data?.authors[0]?.profile_name} ${data?.authors[0]?.verified ? badge : ''}</p>
+                            <p class="my-2 flex gap-2">${data?.authors[0]?.profile_name} ${data?.authors[0]?.verified ? badge : ''}</p>
                             <p class="">${data?.others?.views} views</p>
                     </div>
             </div>
-            Shoes!
-            
-            <div class="badge badge-secondary">NEW</div>
-          </h2>
-          <div class="card-actions justify-end">
-            <div class="badge badge-outline">Fashion</div> 
-            <div class="badge badge-outline">Products</div>
-          </div>
         </div>
       </div>
           `;
