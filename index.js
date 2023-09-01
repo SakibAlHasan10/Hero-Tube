467530dd487108996fedac6a3749bc3cf4b778e5
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-01 10:59:07 +0600

    add blank page

--- index.js
+++ index.js
@@ -32,10 +32,18 @@ const handelAllCategory = async (id) => {
   handleCard(data);
 };
 // handle card
+const allCartContainer = document.getElementById("all-card");
+const noDataAvailable = document.getElementById('no-data-available')
 const handleCard = (card) => {
-    const allCartContainer = document.getElementById("all-card");
     const badge = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-blue-600"><path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" /></svg>'
-    console.log('my', card.length===0 ? 'bangladesh' : 'false')
+    if(card.length===0){
+      noDataCategory()
+    }else{
+      if(card.length > 0){
+    noDataAvailable.innerHTML= '';
+      }
+    }
+    // console.log('my', card.length===0 ? 'bangladesh' : 'false')
     allCartContainer.innerHTML= '';
     card.forEach((data) => {
     const div = document.createElement("div");
@@ -59,6 +67,20 @@ const handleCard = (card) => {
     console.log("hello", data);
   });
 };
+
+// no data category
+const noDataCategory = () =>{
+  const div = document.createElement('div');
+  div.innerHTML= `
+  <div class=" mt-16">
+        <img src="./image/Icon.png" class=" w-32 h-32 mx-auto"/>
+        <h2 class=" text-3xl mt-6 font-bold">Oops!! Sorry, There is no <br> content here</h2>
+  </div>
+  `
+  noDataAvailable.appendChild(div);
+  // allCartContainer.innerHTML= '';
+  console.log('mun', div)
+}
 
 handelAllData();
 handelAllCategory('1000')
